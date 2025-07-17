f3b1fd570b35dadde28f0e450924642ff6e70689
Joey <jowhiey@gmail.com>
2025-07-17 11:08:41 -0230

    Initial Commit
    Threw together two classes for using with the file/DB manipulation.

---
+++ src/StandUser.java
@@ -0,0 +1,74 @@
+public class StandUser {
+    private static int count = 1;
+    private String firstName, lastName, standName;
+    private int age, fromPart, ID;
+
+
+    //Constructors
+    public StandUser(String firstName, String lastName, String standName, int age, int fromPart) {
+        this.ID = count++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.standName = standName;
+        this.age = age;
+        this.fromPart = fromPart;
+    }
+
+    public StandUser() {
+        this.ID = count++;
+    }
+
+
+    //Instance Methods
+    public String toString() {
+        return String.format("\n   ID: %d\n   First Name: %s\n   Last Name: %s\n   Stand Name: %s\n   Age: %d\n   From Part: %d\n", this.ID, this.firstName, this.lastName, this.standName, this.age, this.fromPart);
+    }
+
+
+    //Getter Methods
+    public int getID() {
+        return this.ID;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getStandName() {
+        return this.standName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getFromPart() {
+        return this.fromPart;
+    }
+
+
+    //Setter Methods
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setStandName(String standName) {
+        this.standName = standName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFromPart(int fromPart) {
+        this.fromPart = fromPart;
+    }
+}
---
+++ src/Villain.java
@@ -0,0 +1,53 @@
+public class Villain extends StandUser {
+    private String occupation, group, reason;
+
+
+    //Constructors
+    public Villain(String firstName, String lastName, String standName, int age, int fromPart, String occupation, String group, String reason) {
+        super(firstName, lastName, standName, age, fromPart);
+        this.occupation = occupation;
+        this.group = group;
+        this.reason = reason;
+    }
+
+    public Villain() {
+        super();
+        this.occupation = "Unemployed";
+        this.group = "Unaffiliated";
+        this.reason = "Unknown";
+    }
+
+
+    //Instance Methods
+    public String toString() {
+        return String.format("[Villain]%s   Occupation: %s\n   Group: %s\n   Reason: \"%s\"\n", super.toString(), this.occupation, this.group, this.reason);
+    }
+
+
+    //Getter Methods
+    public String getOccupation() {
+        return this.occupation;
+    }
+
+    public String getGroup() {
+        return this.group;
+    }
+
+    public String getReason() {
+        return this.reason;
+    }
+
+
+    //Setter Methods
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
